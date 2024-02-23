4895040b4820ec09205befc668100480e71d2dcd
Jiahui Xu <jxu@ethz.ch>
2024-02-23 17:03:49 +0100

    update default HLS script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 set-dynamatic-path  ./dynamatic
 
-set-src             ./dynamatic/integration-test/share_test_1/share_test_1.c
+set-src             ./dynamatic/integration-test/matvec/matvec.c
 
 compile
 
