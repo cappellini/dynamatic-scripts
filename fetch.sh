e013db86fbd489b045836abeb899ddc20f542e11
Jiahui Xu <jxu@ethz.ch>
2024-02-24 11:51:43 +0100

    update compilation script to point to the llvm-toolset

--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 #--------------------------------------------------------------------
-#- this script sets up and build everything of legacy-dynamatic
+#- this script clones and builds dynamatic in the official repo
 #--------------------------------------------------------------------
 
-# this script should be called from dynamatic's source directory
 SCRIPT_CWD=$PWD
-
 cd $SCRIPT_CWD
 
 set -e
@@ -15,10 +13,8 @@ DYNAMATIC_ROOT=$SCRIPT_CWD/dynamatic
 
 mkdir -p $DYNAMATIC_ROOT
 
-# git clone git@github.com:epfl-lap/dynamatic.git $DYNAMATIC_ROOT
-git clone git@github.com:Jiahui17/dynamatic-mlir.git $DYNAMATIC_ROOT
+git clone git@github.com:epfl-lap/dynamatic.git $DYNAMATIC_ROOT
+
 cd $DYNAMATIC_ROOT
-git fetch origin test-experimental-integration
-git checkout test-experimental-integration || exit
 
 bash $SCRIPT_CWD/mybuild.sh && echo "successfully built dynamatic"
